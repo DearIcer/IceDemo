cdc842ae1256772415bf4a3904f6412c5311b6f3
DearIce <56225620+DearIcer@users.noreply.github.com>
2023-12-04 21:58:50 +0800

    项目初始化

---
+++ Source/IceDemo/IceDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IceDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ICEDEMO_API AIceDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
